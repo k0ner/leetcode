695998c95574ebfeab452fbac03f74f6902ec45f
k0ner <konradtendera@gmail.com>
2023-01-02 15:59:22 -0800

    509 fibonacci number closed form O(logn)

--- solutions/dyn-prog/day-1/509-fibonacci-number.py
+++ solutions/dyn-prog/day-1/509-fibonacci-number.py
@@ -16,12 +16,24 @@ class Solution(object):
 
         return current
 
+    def fibClosedForm(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        import math
+        sqrt5 = math.sqrt(5)
+        phi = (1 + sqrt5) / 2
+        psi = (1 - sqrt5) / 2
+        return int(round((pow(phi, n) + pow(psi, n)) / sqrt5))
+
+
 if __name__ == "__main__":
-    print(Solution().fib(n=0))
-    print(Solution().fib(n=1))
-    print(Solution().fib(n=2))
-    print(Solution().fib(n=3))
-    print(Solution().fib(n=4))
-    print(Solution().fib(n=5))
-    print(Solution().fib(n=6))
-    print(Solution().fib(n=100))
+    print(Solution().fibClosedForm(n=0))
+    print(Solution().fibClosedForm(n=1))
+    print(Solution().fibClosedForm(n=2))
+    print(Solution().fibClosedForm(n=3))
+    print(Solution().fibClosedForm(n=4))
+    print(Solution().fibClosedForm(n=5))
+    print(Solution().fibClosedForm(n=6))
+    print(Solution().fibClosedForm(n=100))
