0e621ec46a60cca7559310d33bc6494d86785b82
k0ner <konradtendera@gmail.com>
2023-01-08 16:47:01 -0800

    876 middle of linked list

--- solutions/grindcode-75/876-middle-of-linked-list.py
+++ solutions/grindcode-75/876-middle-of-linked-list.py
@@ -1,9 +1,20 @@
 from typing import Optional
 
 from solutions.utils import *
+
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head.next
+
+        while fast:
+            slow = slow.next
+            fast = fast.next.next if fast.next else None
+
+        return slow
 
 
 if __name__ == '__main__':
-    printList(ListNode.)
+    printList(Solution().middleNode(ListNode.from_list(l=[1, 2, 3, 4, 5])))
+    printList(Solution().middleNode((ListNode.from_list(l=[1, 2, 3, 4, 5, 6]))))
+    printList(Solution().middleNode(ListNode.from_list(l=[1])))
